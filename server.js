4bc676d49bf8b5e8200153f255340201cc025819
Chesney Julian <chesney.julian724@gmail.com>
2023-06-29 20:42:51 -0400

    import fs and db.json file; begin api get request

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require('express');
 // import path
 const path = require('path');
+// import fs for file reading/writing
+const fs = require('fs');
+// import db.json file
+const db = require('db/db.json');
 // set up port configuration so it will work with heroku
 const PORT = process.env.PORT ?? 3001;
 // set up instance of express as app
@@ -13,6 +17,11 @@ app.use(express.static('public'));
 // GET /notes returns notes.html
 app.get('/notes', (req, res) => {
     return res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    const readNotes = fs.readFileSync(db);
+    return res.json(readNotes);
 });
 
 //  GET * returns index.html; placed at bottom so wildcard does not replace other paths
