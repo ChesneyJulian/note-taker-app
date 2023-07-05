b97df56a23c722e516d5b8eb191cd9c1ac9fa1a2
Chesney Julian <chesney.julian724@gmail.com>
2023-07-05 16:07:21 -0400

    add comments and clean up server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 // import db.json file
 const db = require('./db/db.json');
-
+// import uuid v4 from express
 const { v4: uuidv4 } = require('uuid');
 // set up port configuration so it will work with heroku
 const PORT = process.env.PORT ?? 3001;
@@ -32,26 +32,36 @@ app.get('/api/notes', (req, res) => {
 // POST to /api/notes adds a note to the db.json file 
 app.post('/api/notes', (req, res) => {
     console.log(`${req.method} request received to add note.`);
+    // trim input for title and text to be used in newEntry
     const title = req.body.title.trim();
     const text = req.body.text.trim();
+    // create new entry using title, text, and express uuid v4
     const newEntry = {
         title,
         text,
         id: uuidv4()
     }
     console.log(newEntry);
-        const original = fs.readFileSync('db/db.json', 'utf8');
-        const parsedNotes = JSON.parse(original);
-        parsedNotes.push(newEntry);
-        const stringNotes = JSON.stringify(parsedNotes, null, 4);
+    // read original notes from db file
+    const original = fs.readFileSync('db/db.json', 'utf8');
+    // parse original db file into json
+    const parsedNotes = JSON.parse(original);
+    // push newEntry into parsed notes to add newest note to json
+    parsedNotes.push(newEntry);
+    // convert parsed notes into formatted string
+    const stringNotes = JSON.stringify(parsedNotes, null, 4);
 
-        fs.writeFile('db/db.json', stringNotes, (err) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log(`New note '${req.body.title}: ${req.body.text}' added successfully!`);
-            }
-        })
+    // write file to db.json using stringified notes 
+    fs.writeFile('db/db.json', stringNotes, (err) => {
+        // add conditional to console.error if err occurs
+        if (err) {
+            console.error(err);
+        } else {
+            // if err doesnt occur, console log message describing the note added to db.json
+            console.log(`New note '${req.body.title}: ${req.body.text}' added successfully!`);
+        }
+    })
+    // return newEntry in json
     return res.json(newEntry);
     }
      
